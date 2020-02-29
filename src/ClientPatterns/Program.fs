6ebc0d8e522ba001027227094fefcea8f1a22d03
Morgan Kenyon <morganwkenyon@gmail.com>
2020-02-29 15:04:42 -0600

    Pushing Code

---
+++ src/ClientPatterns/Program.fs
@@ -0,0 +1,56 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Net.Http
+open Newtonsoft.Json
+open System.Text
+
+[<CLIMutable>]
+type User =
+    {
+        Username : string
+        Password : string
+    }
+
+let getAsync (client:HttpClient) (url:string) = 
+    async {
+        let! response = client.GetAsync(url) |> Async.AwaitTask
+        response.EnsureSuccessStatusCode () |> ignore
+        let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        return content
+    }
+
+let postAsync (client:HttpClient) (url:string) (user:User) =
+    async {
+        let json = JsonConvert.SerializeObject(user)
+        use content = new StringContent(json, Encoding.UTF8, "application/json")
+        let! response = client.PostAsync(url, content) |> Async.AwaitTask
+        let! body = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        return match response.IsSuccessStatusCode with 
+        | true -> Ok body
+        | false -> Error body
+    }
+
+let runExamples =
+    async {
+        let url = "http://localhost:5000/users"
+        use httpClient = new System.Net.Http.HttpClient()
+        let! user = 
+            getAsync httpClient url
+        printfn "Returned User: %s" user
+
+        let newUser = { Username = "Username2"; Password = "SecretSecret" }
+        let! userResult = postAsync httpClient url newUser
+
+        match userResult with
+        | Ok username -> printfn "Created Username: %s" username
+        | Error err -> printfn "Failed request: %s" err
+    }
+    
+
+[<EntryPoint>]
+let main argv =
+    runExamples
+    |> Async.RunSynchronously
+
+    0
---
+++ testApi/TestApi/Program.fs
@@ -0,0 +1,62 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+open FSharp.Control.Tasks.V2.ContextInsensitive
+open Microsoft.AspNetCore.Http
+
+[<CLIMutable>]
+type User =
+    {
+        Username : string
+        Password : string
+    }
+
+let getUser : HttpHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let user = { Username = "Test"; Password = "secret" }
+
+            return! Successful.OK user next ctx
+        }
+
+let addUser =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let! user = ctx.BindJsonAsync<User>();
+
+            return! Successful.OK user.Username next ctx
+        }
+
+let webApp =
+    choose [
+        GET >=>
+            choose [
+                route "/users" >=> getUser
+            ]
+        POST >=>
+            choose [
+                route "/users" >=> addUser
+            ]
+        setStatusCode 404 >=> text "Not Found" ]
+
+let configureApp (app : IApplicationBuilder) =
+    // Add Giraffe to the ASP.NET Core pipeline
+    app.UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    // Add Giraffe dependencies
+    services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main _ =
+    WebHostBuilder()
+        .UseKestrel()
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .ConfigureServices(configureServices)
+        .Build()
+        .Run()
+    0
